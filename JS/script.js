b10067f0a3e02c5a9679afbca8ef534ba93f7e85
FaithKwamboka <okongofaith3@gmail.com>
2022-03-10 02:08:29 +0300

    adding effects to image 1 and 2

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $(".image-1").click(function(){
+      $(".image-1").hide(1000);
+      $(".work-1").toggle(1000);
+      $(".work-1").show(1000);
+    })
+    $(".work-1").click(function(){
+      $(".work-1").hide(1000);
+      $(".image-1").toggle(1000);
+      $(".image-1").show(1000);
+    });
+
+    $(".image-2").click(function(){
+        $(".image-2").hide(1000);
+        $(".work-2").toggle(1000);
+        $(".work-2").show(1000);
+      })
+      $(".work-2").click(function(){
+        $(".work-2").hide(1000);
+        $(".image-2").toggle(1000);
+        $(".image-2").show(1000);
+      });
+
+  });
